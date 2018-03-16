fa877f20e8d19f42280092f23df3152e489fe2eb
wabernathy96 <wabernathy96@gmail.com>
2018-03-16 18:29:56 -0500

    Needs to be cleaned up

--- logic.js
+++ logic.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // Global Variables
     var tDb = firebase.database();
     
-
+    
     // Event listener for form
     $("#train-submit").on("click", function(e){
         
@@ -24,39 +24,41 @@ $(document).ready(function() {
 
         tDb.ref().push(newT);
 
-    
-    
-        // Time conversion event listener
-        tDb.ref().on("child_added", function(childSnapshot){
-            
-            var tSnap = childSnapshot.val();
-            console.log(tSnap);
-
-            var dbName = tSnap.name;
-            var dbDest = tSnap.Destination;
-            var dbFreq = tSnap.Frequency;
-            var dbTimeInput = tSnap.Time_Input;
-            
-            var timeDiff = moment().diff(moment.unix(dbTimeInput), "minutes");
-            var timeRem = moment().diff(moment.unix(dbTimeInput), "minutes") % dbFreq ;
-            var min = dbFreq - timeRem;
-
-            var tArrival = moment().add(min, "m").format("hh:mm A"); 
-            
-            
-            console.log(min);
-            console.log(tArrival);
-            console.log(moment().format("hh:mm A"));
-            console.log(tArrival);
-            console.log(moment().format("X"));
-
-
-
-            var addRow = $("<tr>");
-            var addTData = $("<td>" + tName + "</td>" + "<td>" + tDest + "</td>"+ "<td>" + tFreq + "</td>" + "<td>" + tArrival + "</td>" + "<td>" + min + "</td>");
-
-            addRow.append(addTData);
-            $("#table-body").append(addRow);
-        }); 
     });
+
+    
+    // Time conversion event listener
+    tDb.ref().on("child_added", function(childSnapshot){
+            
+        var tSnap = childSnapshot.val();
+        console.log(tSnap);
+
+        var dbName = tSnap.Name;
+        var dbDest = tSnap.Destination;
+        var dbFreq = tSnap.Frequency;
+        var dbTimeInput = tSnap.Time_Input;
+        
+        var timeDiff = moment().diff(moment.unix(dbTimeInput), "minutes");
+        var timeRem = moment().diff(moment.unix(dbTimeInput), "minutes") % dbFreq ;
+        var min = dbFreq - timeRem;
+
+        var tArrival = moment().add(min, "m").format("hh:mm A"); 
+        
+        
+        console.log(min);
+        console.log(tArrival);
+        console.log(moment().format("hh:mm A"));
+        console.log(tArrival);
+        console.log(moment().format("X"));
+
+
+
+        
+        var addRow = $("<tr>");
+        var addTData = $("<td>" + dbName + "</td>" + "<td>" + dbDest + "</td>"+ "<td>" + dbFreq + "</td>" + "<td>" + tArrival + "</td>" + "<td>" + min + "</td>");
+
+        addRow.append(addTData);
+        $("#table-body").append(addRow);
+        
+    }); 
 });
